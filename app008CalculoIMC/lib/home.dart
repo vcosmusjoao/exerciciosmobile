297128cfe40c3b983ceb1b67c28b5ff5acb32e6f
João Victor Costa Santos <rm88372@fiap.com.br>
2022-04-06 19:13:44 -0300

    Adcionando mais 3 apps do dia 30/03.

---
+++ app008CalculoIMC/lib/home.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String mostrarNumero = "";
+  String titulo = "Pense em um n° de 0 à 10";
+
+  void _GetNumeroAleatorio() {
+    setState(() {
+      int numeroAleatorio = new Random().nextInt(11);
+      mostrarNumero = '$numeroAleatorio';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _titulo(),
+      backgroundColor: Colors.white,
+      body: _corpo(),
+    );
+  }
+
+  _titulo() {
+    return AppBar(
+      title: Text("Charada"),
+      centerTitle: true,
+      backgroundColor: Colors.green,
+    );
+  }
+
+  _corpo() {
+    return SingleChildScrollView(
+      padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          _texto(titulo),
+          _botao(),
+          _texto(mostrarNumero),
+        ],
+      ),
+    );
+  }
+
+  _campo(labelTitulo, objController) {
+    return TextField(
+      keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+          labelText: labelTitulo, labelStyle: TextStyle(color: Colors.green)),
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.green, fontSize: 25.0),
+      controller: objController,
+    );
+  }
+
+  _botao() {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
+      child: Container(
+        height: 50.0,
+        child: RaisedButton(
+          onPressed: _GetNumeroAleatorio,
+          child: Text("Sortear",
+              style: TextStyle(color: Colors.white, fontSize: 20.0)),
+          color: Colors.blue,
+        ),
+      ),
+    );
+  }
+
+  _texto(textoParaExibir) {
+    return Text(
+      textoParaExibir,
+      textAlign: TextAlign.center,
+      style: TextStyle(color: Colors.red, fontSize: 25.0),
+    );
+  }
+}
